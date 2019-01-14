f0741ca4a2fba3d3519cedf81883f49cbbde09dc
Isac <isac_asj91@hotmail.com>
2019-01-14 21:18:32 -0200

    update eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    "rules": {
+        "semi": ["error", "never"],
+        "react/jsx-filename-extension": "off",
+        "react/jsx-one-expression-per-line": "off",
+        "react/prefer-stateless-function": "off",
+        "import/no-named-as-default": "off",
+        "import/no-named-as-default-member": "off",
+        "object-curly-newline": "off"
+    }
+};
